383f0ec33ec851e82bfed92eae124cf5449c344f
Pushkal <pushkalvaidya1@gmail.com>
2024-01-08 18:43:33 +0530

    Initial Commit

--- src/main/java/com/pksv/binarytree/InvertBinaryTree.java
+++ src/main/java/com/pksv/binarytree/InvertBinaryTree.java
@@ -22,7 +22,7 @@ public class InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
         if(root == null) return null;
         root.left = invertTree(root.right);
-        root.right = invertTree(root.left);;
+        root.right = invertTree(root.left);
         return root;
     }
 }
