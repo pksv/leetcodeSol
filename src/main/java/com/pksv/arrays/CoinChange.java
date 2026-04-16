274d198a2cf7a57eb9b642716bfd2dac9e9b8df5
pushkal <pushkalvaidya1@gmail.com>
2026-04-16 15:18:44 +0530

    Initial Commit

--- src/main/java/com/pksv/arrays/CoinChange.java
+++ src/main/java/com/pksv/arrays/CoinChange.java
@@ -10,24 +10,10 @@ public class CoinChange {
 //        Output: 3
         int[] coins = {186, 419, 83, 408};
         int amount = 6249;
-        System.out.println(new CoinChange().coinChange2(coins, amount));
+        System.out.println(new CoinChange().coinChange(coins, amount));
     }
 
     public int coinChange(int[] coins, int amount) {
-        Arrays.sort(coins);
-        int rem = amount;
-        int noOfCoins = 0;
-        for (int i = coins.length - 1; i >= 0 && rem > 0; i--) {
-            if (rem >= coins[i]) {
-                System.out.println(coins[i] + " " + (rem / coins[i]));
-                noOfCoins += (rem / coins[i]);
-                rem = rem % coins[i];
-            }
-        }
-        return rem == 0 ? noOfCoins : -1;
-    }
-
-    public int coinChange2(int[] coins, int amount) {
         if (amount == 0) return 0;
         int[] res = new int[amount + 1];
         int leastCoin = amount;
